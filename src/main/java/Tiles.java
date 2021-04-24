ff3e1a60dd716012378adbdbf56734a1b545ce74
bhusasub000 <72724784+bhusasub000@users.noreply.github.com>
2021-04-24 15:34:17 -0500

    1

--- src/main/java/Tiles.java
+++ src/main/java/Tiles.java
@@ -1,5 +1,21 @@
 import java.awt.Button;
 
 public class Tiles extends Button {
-
+	private int row, column, number;
+	private boolean disabled;
+	
+	Tiles(int row, int column, int number) {
+		this.row = row;
+		this.column = column;
+		this.disabled = true;
+		this.number = number;
+	}
+	
+	public boolean isMoveable(Tiles[][] puzzle, int row, int column) {
+		// if the tile has a neighbor whose number == 0, then moveable otherwise not
+		return false;
+	}
+	
+	
+	
 }
